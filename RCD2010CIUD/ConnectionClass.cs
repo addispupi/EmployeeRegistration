13c75b4e1f740594c497d76af58bdf54cf3a35a7
Addisu <72408025+addispupi@users.noreply.github.com>
2021-04-06 11:57:19 +0300

    extra employee dataset uploaded

---
+++ RCD2010CIUD/ConnectionClass.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace RCD2010CIUD
+{
+    class ConnectionClass
+    {
+        SqlConnection dbconnection;
+        string conString;
+
+        public SqlConnection Connection()
+        {
+            conString = @"Data Source=GASHAASE;Initial Catalog=employeeC;Integrated Security=True";
+            dbconnection = new SqlConnection(conString);
+            try
+            {
+                dbconnection.Open();
+                //MessageBox.Show("Dtabase has Connected Successfully!");
+                return dbconnection;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+    }
+}
